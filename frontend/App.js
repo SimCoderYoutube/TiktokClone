c7ed8b568a38ac11fde886505d8d904b8bcc4e27
SimCoderYoutube <simplecodervideos@gmail.com>
2021-07-18 19:34:49 +0100

    [Add] route system

--- frontend/App.js
+++ frontend/App.js
@@ -1,36 +1,29 @@
-import React from 'react';
+import Constants from 'expo-constants';
 import * as firebase from 'firebase';
-import { StyleSheet, Text, View } from 'react-native';
-
-
-import { Provider } from 'react-redux'
-import { createStore, applyMiddleware } from 'redux'
-import rootReducer from './src/redux/reducers'
-import thunk from 'redux-thunk'
-const store = createStore(rootReducer, applyMiddleware(thunk))
-
-
-import Constants from 'expo-constants'
+import React from 'react';
+import { Provider } from 'react-redux';
+import { applyMiddleware, createStore } from 'redux';
+import thunk from 'redux-thunk';
+import Route from './src/navigation/main';
+import rootReducer from './src/redux/reducers';
 import AuthScreen from './src/screens/auth';
 
 
-if (firebase.apps.lenght === 0) {
+const store = createStore(rootReducer, applyMiddleware(thunk))
+
+
+
+
+if (firebase.apps.length === 0) {
   firebase.initializeApp(Constants.manifest.web.config.firebase)
+} else {
+  firebase.app()
 }
 
 export default function App() {
   return (
     <Provider store={store} >
-      <AuthScreen />
+      <Route />
     </Provider>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
--- frontend/src/redux/actions/auth.js
+++ frontend/src/redux/actions/auth.js
@@ -2,6 +2,33 @@
 import firebase from 'firebase'
 require('firebase/firebase-auth')
 
+import { USER_STATE_CHANGE } from '../constants'
+
+export const userAuthStateListener = () => dispatch => {
+    firebase.auth().onAuthStateChanged((user) => {
+        if (user) {
+            dispatch(getCurrentUserData())
+        } else {
+            dispatch({ type: USER_STATE_CHANGE, currentUser: null, loaded: true })
+        }
+    })
+}
+
+export const getCurrentUserData = () => dispatch => {
+    firebase.firestore()
+        .collection('user')
+        .doc(firebase.auth().currentUser.uid)
+        .onSnapshot((res) => {
+            if (res.exists) {
+                return dispatch({
+                    type: USER_STATE_CHANGE,
+                    currentUser: res.data(),
+                    loaded: true
+                })
+            }
+        })
+}
+
 export const login = (email, password) => dispatch => new Promise((resolve, reject) => {
     firebase.auth().signInWithEmailAndPassword(email, password)
         .then(() => {
