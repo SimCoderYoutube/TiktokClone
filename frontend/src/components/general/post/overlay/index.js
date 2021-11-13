1e92fd85cc239670e99a98170999798120cb615e
SimCoderYoutube <simplecodervideos@gmail.com>
2021-11-13 14:02:21 +0000

    [Add] documentation to the new code

--- frontend/src/components/general/post/overlay/index.js
+++ frontend/src/components/general/post/overlay/index.js
@@ -1,60 +1,79 @@
-import React, { useEffect, useMemo, useState } from 'react'
-import { View, Text, Image, TouchableOpacity } from 'react-native'
-import styles from './styles'
-import { Ionicons } from '@expo/vector-icons'
-import { getLikeById, updateLike } from '../../../../services/posts'
-import { useSelector } from 'react-redux'
-import { throttle } from 'throttle-debounce'
+import React, { useEffect, useMemo, useState } from "react";
+import { View, Text, Image, TouchableOpacity } from "react-native";
+import styles from "./styles";
+import { Ionicons } from "@expo/vector-icons";
+import { getLikeById, updateLike } from "../../../../services/posts";
+import { useSelector } from "react-redux";
+import { throttle } from "throttle-debounce";
 
 /**
  * Function that renders a component meant to be overlapped on
  * top of the post with the post info like user's display name and avatar
  * and the post's description
- * 
- * @param {Object} user that created the post 
- * @param {Object} post object 
+ *
+ * @param {Object} user that created the post
+ * @param {Object} post object
  */
 export default function PostSingleOverlay({ user, post }) {
-    const currentUser = useSelector((state) => state.auth.currentUser)
-    const [currentLikeState, setCurrentLikeState] = useState({ state: false, counter: post.likesCount })
+  const currentUser = useSelector((state) => state.auth.currentUser);
+  const [currentLikeState, setCurrentLikeState] = useState({
+    state: false,
+    counter: post.likesCount,
+  });
 
+  useEffect(() => {
+    getLikeById(post.id, currentUser.uid).then((res) => {
+      setCurrentLikeState({
+        ...currentLikeState,
+        state: res,
+      });
+    });
+  }, []);
 
-    useEffect(() => {
-        getLikeById(post.id, currentUser.uid).then((res) => {
-            setCurrentLikeState({
-                ...currentLikeState,
-                state: res,
-            })
-        })
-    }, [])
+  /**
+   * Handles the like button action.
+   *
+   * In order to make the action more snappy the like action
+   * is optimistic, meaning we don't wait for a response from the
+   * server and always assume the write/delete action is successful
+   */
+  const handleUpdateLike = useMemo(
+    () =>
+      throttle(500, true, (currentLikeStateInst) => {
+        setCurrentLikeState({
+          state: !currentLikeStateInst.state,
+          counter:
+            currentLikeStateInst.counter +
+            (currentLikeStateInst.state ? -1 : 1),
+        });
+        updateLike(post.id, currentUser.uid, currentLikeStateInst.state);
+      }),
+    []
+  );
 
+  return (
+    <View style={styles.container}>
+      <View>
+        <Text style={styles.displayName}>{user?.displayName}</Text>
+        <Text style={styles.description}>{post.description}</Text>
+      </View>
 
-    const handleUpdateLike = useMemo(
-        () =>
-            throttle(500, true, (currentLikeStateInst) => {
-                setCurrentLikeState({
-                    state: !currentLikeStateInst.state,
-                    counter: currentLikeStateInst.counter + (currentLikeStateInst.state ? -1 : 1)
-                })
-                updateLike(post.id, currentUser.uid, currentLikeStateInst.state)
-            })
-        , []
-    )
-
-    return (
-        <View style={styles.container}>
-            <View>
-                <Text style={styles.displayName}>{user?.displayName}</Text>
-                <Text style={styles.description}>{post.description}</Text>
-            </View>
-
-            <View style={styles.leftContainer}>
-                <Image style={styles.avatar} source={{ uri: user?.photoURL }} />
-                <TouchableOpacity style={styles.actionButton} onPress={() => handleUpdateLike(currentLikeState)}>
-                    <Ionicons color='white' size={40} name={currentLikeState.state ? 'heart' : "heart-outline"} />
-                    <Text style={styles.actionButtonText}>{currentLikeState.counter}</Text>
-                </TouchableOpacity>
-            </View>
-        </View>
-    )
+      <View style={styles.leftContainer}>
+        <Image style={styles.avatar} source={{ uri: user?.photoURL }} />
+        <TouchableOpacity
+          style={styles.actionButton}
+          onPress={() => handleUpdateLike(currentLikeState)}
+        >
+          <Ionicons
+            color="white"
+            size={40}
+            name={currentLikeState.state ? "heart" : "heart-outline"}
+          />
+          <Text style={styles.actionButtonText}>
+            {currentLikeState.counter}
+          </Text>
+        </TouchableOpacity>
+      </View>
+    </View>
+  );
 }
--- frontend/src/services/posts.js
+++ frontend/src/services/posts.js
@@ -1,53 +1,67 @@
-import firebase from 'firebase'
+import firebase from "firebase";
 
 /**
  * Returns all the posts in the database.
- * 
+ *
  * @returns {Promise<[<Object>]>} post list if successful.
  */
-export const getFeed = () => new Promise((resolve, reject) => {
+export const getFeed = () =>
+  new Promise((resolve, reject) => {
     firebase
-        .firestore()
-        .collection('post')
-        .get()
-        .then((res) => {
-            let posts = res.docs.map((value) => {
-                const id = value.id;
-                const data = value.data();
-                return { id, ...data }
-            })
-            resolve(posts)
-        })
-})
+      .firestore()
+      .collection("post")
+      .get()
+      .then((res) => {
+        let posts = res.docs.map((value) => {
+          const id = value.id;
+          const data = value.data();
+          return { id, ...data };
+        });
+        resolve(posts);
+      });
+  });
 
-export const getLikeById = (postId, uid) => new Promise((resolve, reject) => {
+/**
+ * Gets the like state of a user in a specific post
+ * @param {String} postId - id of the post
+ * @param {String} uid - id of the user to get the like state of.
+ *
+ * @returns {Promise<Boolean>} true if user likes it and vice versa.
+ */
+export const getLikeById = (postId, uid) =>
+  new Promise((resolve, reject) => {
     firebase
-        .firestore()
-        .collection('post')
-        .doc(postId)
-        .collection('likes')
-        .doc(uid)
-        .get()
-        .then((res) => resolve(res.exists))
-})
+      .firestore()
+      .collection("post")
+      .doc(postId)
+      .collection("likes")
+      .doc(uid)
+      .get()
+      .then((res) => resolve(res.exists));
+  });
 
-export const updateLike = (postId, uid, currentLikeState) => new Promise((resolve, reject) => {
-    if (currentLikeState) {
-        firebase
-            .firestore()
-            .collection('post')
-            .doc(postId)
-            .collection('likes')
-            .doc(uid)
-            .delete()
-    } else {
-        firebase
-            .firestore()
-            .collection('post')
-            .doc(postId)
-            .collection('likes')
-            .doc(uid)
-            .set({})
-    }
-
-})
+/**
+ * Updates the like of a post according to the current user's id
+ * @param {String} postId - id of the post
+ * @param {String} uid - id of the user to get the like state of.
+ * @param {Boolean} currentLikeState - true if the user likes the post and vice versa.
+ */
+export const updateLike = (postId, uid, currentLikeState) => {
+  if (currentLikeState) {
+    firebase
+      .firestore()
+      .collection("post")
+      .doc(postId)
+      .collection("likes")
+      .doc(uid)
+      .delete();
+  } else {
+    firebase
+      .firestore()
+      .collection("post")
+      .doc(postId)
+      .collection("likes")
+      .doc(uid)
+      .set({});
+  }
+};
